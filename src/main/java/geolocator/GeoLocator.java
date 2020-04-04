5f9fef7481b4f5bce9aa06e780b52920c8d235bc
Muhammad Hassan Raza <sshthassanraza@hotmail.com>
2020-04-04 16:34:16 +0200

    log assignmnet commit

--- src/main/java/geolocator/GeoLocator.java
+++ src/main/java/geolocator/GeoLocator.java
@@ -1,5 +1,6 @@
 package geolocator;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import java.io.IOException;
@@ -9,11 +10,14 @@ import com.google.gson.Gson;
 import com.google.common.net.UrlEscapers;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GeoLocator {
 
     public static final String GEOLOCATOR_SERVICE_URI = "http://ip-api.com/json/";
 
+    private static Logger logger = LoggerFactory.getLogger(GeoLocator.class);
     private static Gson GSON = new Gson();
 
     public GeoLocator() {}
@@ -22,25 +26,41 @@ public class GeoLocator {
         return getGeoLocation(null);
     }
 
-    public GeoLocation getGeoLocation(String ipAddrOrHost) throws IOException {
-        URL url;
+    public GeoLocation getGeoLocation(String ipAddrOrHost)  {
+
+        URL url = null;
+        String s = new String();
         if (ipAddrOrHost != null) {
             ipAddrOrHost = UrlEscapers.urlPathSegmentEscaper().escape(ipAddrOrHost);
-            url = new URL(GEOLOCATOR_SERVICE_URI + ipAddrOrHost);
+            try {
+                logger.debug("It can be debug message");
+                url = new URL(GEOLOCATOR_SERVICE_URI + ipAddrOrHost);
+            } catch (MalformedURLException e) {
+                logger.error("MalFormed URL", new RuntimeException(e.getMessage()));
+            }
         } else {
-            url = new URL(GEOLOCATOR_SERVICE_URI);
+            try {
+                url = new URL(GEOLOCATOR_SERVICE_URI);
+            } catch (MalformedURLException e) {
+                logger.error("MalFormed URL", new RuntimeException(e.getMessage()));
+            }
         }
-        String s = IOUtils.toString(url, "UTF-8");
+        try {
+            s = IOUtils.toString(url, "UTF-8");
+        }
+         catch (IOException e) {
+             logger.error("IO Exception", new RuntimeException(e.getMessage()));
+        }
+        catch(NullPointerException e){
+            logger.error("url is NULL", new RuntimeException(e.getMessage()));
+        }
         return GSON.fromJson(s, GeoLocation.class);
     }
 
     public static void main(String[] args) throws IOException {
-        try {
-            String arg = args.length > 0 ? args[0] : null;
-            System.out.println(new GeoLocator().getGeoLocation(arg));
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
+        String arg = args.length > 0 ? args[0] : null;
+        logger.info("Printing GeoLocation INFO");
+        System.out.println(new GeoLocator().getGeoLocation(arg));
     }
 
 }
